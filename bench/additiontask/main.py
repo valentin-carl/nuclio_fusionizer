a92ea5483d50386f75416a08b19e22a640e1e11d
valentin-carl <vacleantrinl@gmail.com>
2024-02-10 11:57:11 +0100

    adapt additiontask for sand benchmark

--- bench/additiontask/main.py
+++ bench/additiontask/main.py
@@ -1,2 +1,24 @@
-def handler(context, event, requests_session):
-    return int(event.body["value"]) + 1
+import pika
+
+
+def invoke(function_name: str, payload: any) -> None:
+    connection = pika.BlockingConnection(pika.URLParameters(broker_url))
+    channel = connection.channel()
+    channel.basic_publish(exchange="", routing_key=function_name, body=payload)
+    connection.close()
+
+def handler(context, event):
+
+    target, actual = int(event.body["value"]), 0
+
+    if actual >= target:
+        invoke("entrytask_result", f"{actual}")
+
+    else:
+        invoke("additiontask", f"{actual+1}")
+
+    return context.Response(
+        body="",
+        content_type='text/plain',
+        status_code=200
+    )
