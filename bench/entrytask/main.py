5269fbba1e6ccaf37834fd9a778ef66298e77784
valentin-carl <vacleantrinl@gmail.com>
2024-02-10 11:53:01 +0100

    adapt entrytask for sand benchmark

--- bench/entrytask/main.py
+++ bench/entrytask/main.py
@@ -1,13 +1,46 @@
-def handler(context, event, requests_session):
-    target = int(event.body["value"])
-    actual = 0
+import pika
+import json
 
-    fusionizer_address = event.headers["Fusionizer-Server-Address"]
-    # invoke addition task
-    url = f"http://{fusionizer_address}:8000/additiontask"
-    headers = {"Content-Type": "application/json"}
-    data = {"value": event.body["value"]}
-    
-    while actual < target:
-        response = requests_session.post(url, headers=headers, json=data)
-        actual = int(response.text)
+
+broker_url = "amqp://jeff:jeff@localhost:5672/%2F"
+
+def invoke(function_name: str, payload: any) -> None:
+    connection = pika.BlockingConnection(pika.URLParameters(broker_url))
+    channel = connection.channel()
+    channel.basic_publish(exchange="", routing_key=function_name, body=payload)
+    connection.close()
+
+def handler(context, event):
+
+    target, actual = int(event.body["value"]), 0
+
+    payload = json.dumps({"target": target, "actual": actual})
+    invoke("additiontask", payload)
+
+    def wait_for_result():
+
+        connection = pika.BlockingConnection(pika.URLParameters(broker_url))
+        channel = connection.channel()
+        channel.queue_declare(queue="entrytask_result")
+
+        done = False
+        msg = None
+
+        def callback(channel, method, properties, body):
+            nonlocal done, msg
+            msg = body
+            done = True
+
+        channel.basic_consume(queue="entrytask_result", on_message_callback=callback, auto_ack=True)
+
+        while not done:
+            connection.process_data_events()
+
+        connection.close()
+        return msg
+
+    return context.Response(
+        body=wait_for_result(),
+        content_type='text/plain',
+        status_code=200
+    )
